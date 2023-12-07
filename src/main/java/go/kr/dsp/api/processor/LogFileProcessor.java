939ba07606b07f87239927ad05a9a18d736a2d13
YMS <yms@yms.com>
2023-12-07 17:47:05 +0900

    [12.07]

---
+++ src/main/java/go/kr/dsp/api/processor/LogFileProcessor.java
@@ -0,0 +1,44 @@
+package go.kr.dsp.api.processor;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.camel.Exchange;
+import org.apache.camel.FluentProducerTemplate;
+import org.apache.camel.Processor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class LogFileProcessor implements Processor {
+  private final FluentProducerTemplate fluentProducerTemplate;
+
+  @Value("${spring.config.activate.on-profile}")
+  private String logName;
+
+  @Override
+  public void process(Exchange exchange) throws Exception {
+    String jsonStr = "";
+    ObjectMapper mapper = new ObjectMapper();
+    Map<String, Object> map=new HashMap<>();
+
+    if(exchange.getMessage().getHeader("fileExtension",String.class).equals("sh")){
+      String request = exchange.getMessage().getBody(String.class);
+      map.put("request",request);
+      log.info(request);
+    }
+    String path = "logs/"+logName+".log";
+    String str=Files.readString(Path.of(path));
+
+    map.put("logFile",str);
+    jsonStr = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(map);
+    exchange.getMessage().setBody(jsonStr);
+  }
+}
